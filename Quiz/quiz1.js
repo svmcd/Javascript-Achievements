1fea8ba2f6355be7dc0c070a68cc25a312e1c45c
svmcd <spolat0750@gmail.com>
2020-12-17 10:58:17 +0100

    Les 5 quiz af

---
+++ Quiz/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "",
+        "imageURI": "https://free-icon-rainbow.com/i/icon_04578/icon_045780_256.png"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Hoeveel dagen doet de aarde erover om 1 keer om de zon te draaien?",
+				"answers": [{
+						"answer": "30",
+						"feedback": false
+					},
+					{
+						"answer": "365",
+						"feedback": true
+					},
+					{
+						"answer": "31",
+						"feedback": false
+					},
+					{
+						"answer": "52",
+						"feedback": false
+					},
+					{
+						"answer": "100",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Hoeveel zintuigen heeft een mens?",
+				"answers": [{
+						"answer": "6",
+						"feedback": false
+					},
+					{
+						"answer": "4",
+						"feedback": false
+					},
+					{
+						"answer": "10",
+						"feedback": false
+					},
+					{
+						"answer": "5",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "Welke planeet is de grootste in ons zonnestelsel?",
+				"answers": [{
+						"answer": "Mars",
+						"feedback": false
+					},
+					{
+						"answer": "Jupiter",
+						"feedback": true
+					},
+					{
+						"answer": "Saturnus",
+						"feedback": false
+					},
+					{
+						"answer": "Uranus",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
