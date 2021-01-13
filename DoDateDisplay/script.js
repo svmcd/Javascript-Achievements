8d561cb58a528a85cd7387b00db276969021ba8d
svmcd <spolat0750@gmail.com>
2021-01-13 10:55:44 +0100

    dodatedisplay bewerkt

--- DoDateDisplay/script.js
+++ DoDateDisplay/script.js
@@ -5,7 +5,7 @@ function toonDatum(){
     var maand = document.getElementById("maand")
     var jaar = document.getElementById("jaar")  
     dag.innerHTML = datum.getDate();
-    maand.innerHTML = datum.getMonth();
+    maand.innerHTML = datum.getMonth() +1;
     jaar.innerHTML = datum.getFullYear();
 }
 
